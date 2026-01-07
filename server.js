8d252f4d53b439b5143a07436e809be898dd6a93
trirach1 <trirach@gmail.com>
2026-01-07 10:26:23 +0100

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import NodeCache from 'node-cache';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
